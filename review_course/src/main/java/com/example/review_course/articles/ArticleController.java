c868240f99c1c0f63dbda5154469f82f3aa3856f
Jirawan Chuapradit <jirawan-chuapradit@users.noreply.github.com>
2019-03-04 19:51:18 +0700

    create article
    --------------- use postman google plugin-----------------
    
    ------------https://app.getpostman.com/join-team?invite_code=585257f94d1173ec2d1fb329bf3666f8------

--- review_course/src/main/java/com/example/review_course/articles/ArticleController.java
+++ review_course/src/main/java/com/example/review_course/articles/ArticleController.java
@@ -1,8 +1,7 @@
 package com.example.review_course.articles;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,14 +9,23 @@ import java.util.List;
 @RestController
 public class ArticleController {
 
+    @Autowired
+    private ArticleService articleService;
+
     //create article
 //    @GetMapping("/article")
     @RequestMapping("/articles")
     public List<Article> getAllArticles(){
-        return Arrays.asList(
-                new Article("001", "สังคมศาสตร์", "ECONOMICS FOR BUSINESS","* * * * *","ดีมากๆ เเรียนเกี่ยวกับแนวคิดการทำงานของระบบเศษฐกิจอ่ะไม่มีเช็คชื่อ ส่งงานอย่างเดียว","A"),
-                new Article("002", "สังคมศาสตร์", "ENTREPRENEURSHIP","* * * *","อาจารย์สอนดี แต่เนื้อหาเยอะ ไม่เหมาะกับคนที่ ไม่ชอบอ่านหนังสือ","A"),
-                new Article("003", "ภาษา", "ENGLISH FOR MARKETING","* *","เน้นเวิร์คช้อป ไม่เข้าเรียนได้ ส่วนตัวคิดว่าดี แต่ได้ A ยาก","B")
-                );
+        return articleService.getAllArticles();
+    }
+
+//    @RequestMapping("/articles/{id}")
+//    public Article getArticle(@PathVariable  String id){
+//        return articleService.getArticle(id);
+//    }
+
+    @PostMapping(value = "/articles")
+    public void addArticle(@RequestBody Article article){
+        articleService.addArticle(article);
     }
 }
---
+++ review_course/src/main/java/com/example/review_course/articles/ArticleService.java
@@ -0,0 +1,29 @@
+package com.example.review_course.articles;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ArticleService {
+
+    public List<Article> articles = new ArrayList<>(Arrays.asList(
+            new Article("001", "สังคมศาสตร์", "ECONOMICS FOR BUSINESS","* * * * *","ดีมากๆ เเรียนเกี่ยวกับแนวคิดการทำงานของระบบเศษฐกิจอ่ะไม่มีเช็คชื่อ ส่งงานอย่างเดียว","A"),
+            new Article("002", "สังคมศาสตร์", "ENTREPRENEURSHIP","* * * *","อาจารย์สอนดี แต่เนื้อหาเยอะ ไม่เหมาะกับคนที่ ไม่ชอบอ่านหนังสือ","A"),
+            new Article("003", "ภาษา", "ENGLISH FOR MARKETING","* *","เน้นเวิร์คช้อป ไม่เข้าเรียนได้ ส่วนตัวคิดว่าดี แต่ได้ A ยาก","B")
+    ));
+
+    public List<Article> getAllArticles(){
+        return articles;
+    }
+
+//    public Article getArticle(String id){
+//        return articles.stream().filter(a -> a.getArticleId().equals(id)).findFirst().get();
+//    }
+//
+    public void addArticle(Article article){
+        articles.add(article);
+    }
+}
