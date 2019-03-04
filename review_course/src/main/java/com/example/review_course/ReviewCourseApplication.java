fa8dd2b78b897e72191572f0a1f4c136f133268f
Jirawan Chuapradit <jirawan-chuapradit@users.noreply.github.com>
2019-03-04 17:28:06 +0700

    create Project
    -------------------------------create Project with spring boot------------------------
    
    -------------------- create articles success----------------

---
+++ review_course/src/main/java/com/example/review_course/ReviewCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.review_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewCourseApplication.class, args);
+    }
+
+}
---
+++ review_course/src/test/java/com/example/review_course/ReviewCourseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.review_course;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReviewCourseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
