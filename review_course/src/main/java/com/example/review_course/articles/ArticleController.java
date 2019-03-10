2b9dffd5fe01d7dac621aac9e130f890c9c37191
Adobe Beta <adobebeta1997@gmail.com>
2019-03-11 04:31:54 +0700

    add article controller

--- review_course/src/main/java/com/example/review_course/articles/ArticleController.java
+++ review_course/src/main/java/com/example/review_course/articles/ArticleController.java
@@ -12,20 +12,28 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    //create article
-//    @GetMapping("/article")
-    @RequestMapping("/articles")
-    public List<Article> getAllArticles(){
-        return articleService.getAllArticles();
+    //    get an Article
+    @RequestMapping("/articles/{id}")
+    public Article getArticle(@PathVariable  String id){
+        return articleService.getArticle(id) ;
     }
 
-//    @RequestMapping("/articles/{id}")
-//    public Article getArticle(@PathVariable  String id){
-//        return articleService.getArticle(id);
-//    }
 
+    //create
     @PostMapping(value = "/articles")
     public void addArticle(@RequestBody Article article){
         articleService.addArticle(article);
+    }
+
+    //update
+//    @PutMapping(value = "/articles/{id}")
+//    public void updateArticle(@RequestBody Article article, @PathVariable String id){
+//        articleService.updateArticle(id, article);
+//    }
+
+    //delete
+    @DeleteMapping(value = "/articles/{id}")
+    public void deleteArticle(@PathVariable String id){
+        articleService.deleteArticle(id);
     }
 }
