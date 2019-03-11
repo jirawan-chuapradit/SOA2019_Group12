dab3d7eecc032a0810352dffba93b02886c02de2
59070178 <59070178@kmitl.ac.th>
2019-03-11 21:47:59 +0700

    Create Comments.java
    model

---
+++ review_course/src/main/java/com/example/review_course/articles/model/Comments.java
@@ -0,0 +1,53 @@
+package com.example.review_course.articles.model;
+
+public class Comments {
+
+
+    private String commentId;
+    private String name;
+    private String star;
+    private String description;
+
+
+    public Comments() {
+    }
+
+    public Comments(String commentId, String name, String star, String description) {
+        this.commentId = commentId;
+        this.name = name;
+        this.star = star;
+        this.description = description;
+    }
+
+    public String getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(String commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStar() {
+        return star;
+    }
+
+    public void setStar(String star) {
+        this.star = star;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
