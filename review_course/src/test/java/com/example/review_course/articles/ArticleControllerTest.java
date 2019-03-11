52a3464177d08e9a561240d19fad90f3967e7a64
Adobe Beta <adobebeta1997@gmail.com>
2019-03-11 22:54:15 +0700

    add article controller Test

---
+++ review_course/src/test/java/com/example/review_course/articles/ArticleControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.review_course.articles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class ArticleControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void addArticle() throws Exception {
+
+    }
+
+}
