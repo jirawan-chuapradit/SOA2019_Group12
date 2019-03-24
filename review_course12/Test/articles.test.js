c13aee68674eedc368981014de20974d39d4d9cc
chocolatenate <59070030@it.kmitl.ac.th>
2019-03-24 19:16:18 +0700

    article.test
    -------------  test article getArticlesByID success ---------------------

--- review_course12/Test/articles.test.js
+++ review_course12/Test/articles.test.js
@@ -1,36 +1,49 @@
-//Testing articles
+// Testing articles
 
 const article = require("../articles");
 
-describe("getAllArticles", () => {
-    it("should return all article",  () => {
+// describe("getAllArticles", () => {
+//     it("should return all article",  () => {
 
-        const result = article.findAll();
+//         // const result = article.findAll();
+       
+//         'use strict';
 
-        // 'use strict';
-
-        // const fs = require('fs');
-
-        // fs.readFile('../review_course12/data/mockArticles.json', (err, data) => {
-        //     if (err) throw err;
-        //     let mockArticles  = JSON.parse(data);
-        //     console.log(mockArticles);
-        //     expect(result).toEqual(mockArticles);
-        // });
-
-    
-            expect(result).toEqual(" ");
+//         const fs = require('fs');
+        
+//         let rawdata = fs.readFileSync('../review_course12/data/mockArticles.json');  
+//         let mockArticles = JSON.parse(rawdata);  
+//         console.log(mockArticles);
+//         expect(result).toEqual(mockArticles)
         
 
-    });
-});
+//     });
+// });
 
 
 describe("getArticlesByID", () => {
     it("should return Article By ID",  () => {
 
-        const result = article.findById(1);
-        expect(result).toEqual({"id":1,"category":"สังคมศาสตร์","subject":"ECONOMICS FOR BUSINESS","stars":"* * * * *","description":"ดีมากๆ เเรียนเกี่ยวกับแนวคิดการทำงานของระบบเศษฐกิจอ่ะไม่มีเช็คชื่อ ส่งงานอย่างเดียว","grade":"A"});
+        // const result = article.findById(1);
+       
+        'use strict';
+
+        const fs = require('fs');
+
+        let rawdata = fs.readFileSync('../review_course12/data/mockarticles.json');
+        let mockArticles = JSON.parse(rawdata)
+        console.log(mockArticles)
+
+        var _ = require("underscore");
+        var filtered = _.where(mockArticles, {id: 1});
+        console.log(filtered)
+        
+         expect(filtered).toEqual([
+             {"id":1,"category":"สังคมศาสตร์",
+             "subject":"ECONOMICS FOR BUSINESS",
+             "stars":"* * * * *",
+             "description":"ดีมากๆ เเรียนเกี่ยวกับแนวคิดการทำงานของระบบเศษฐกิจอ่ะไม่มีเช็คชื่อ ส่งงานอย่างเดียว",
+             "grade":"A"}]);
 
 
     });
--- review_course12/Test/comment.test.js
+++ review_course12/Test/comment.test.js
@@ -1,55 +1,68 @@
 //Testing comments
 
-const comment = require('../comment');
+// const comment = require('../comment');
 
-describe('getAllcomment', () => {
-    it('should return all comment', () => {
+// describe('getAllcomment', () => {
+//     it('should return all comment', () => {
 
-        const result = comment.findAll();
+//         // const result = comment.findAll();
+//         // console.log(result)
 
-        // 'use strict';
 
-        // const fs = require('fs');
-        // fs.readFile('../review_course12/data/mockComments.json', (err, data) => {
-        //     if (err) throw err;
-        //     let mockComments = JSON.parse(data);
-        //     console.log(student);
-        //     expect(result).toEqual(mockComments);
-        // });
+//         var _ = require("underscore");
 
-        var fs = require('fs');
-        var obj;
-        fs.readFile('../review_course12/data/mockComments.json', 'utf8', function (err, data) {
-            if (err) throw err;
-            obj = JSON.parse(data);
-            console.log(obj);
-            expect(result).toEqual(obj);
-        });
-    });
-});
+//         'use strict';
 
-// describe('getCommentByArticleID', () => {
-//     it('should return Comments By ID of Article', () => {
+//         const fs = require('fs');
 
-//         const result = comment.findByArticletId(1);
-//         expect(result).toEqual([
-//             {
-//                 "id_comment": 1,
-//                 "id_article": 1,
-//                 "content": "รีวิวดีมากเลยครับ",
-//                 "stars": "* * *",
-//                 "id_profile": 1,
+//         let rawdata = fs.readFileSync('../review_course12/data/mockComments.json');
+//         let mockCommentS = JSON.parse(rawdata);
+//         console.log(mockCommentS);
 
-//             },
-//             {
-//                 "id_comment": 2,
-//                 "id_article": 1,
-//                 "content": "ขอบคุณมากๆ ค้าาา",
-//                 "stars": "* * *",
-//                 "id_profile": 1,
+//         var filtered = _.where(mockCommentS, {id_article: "1"});
+//         console.log(filtered)
 
-//             }
-//         ]);
+//         // expect(result).toEqual(mockCommentS)
+//     });
+// });
+
+describe('getCommentByArticleID', () => {
+    it('should return Comments By ID of Article', () => {
+
+        
+        var _ = require("underscore");
+
+        'use strict';
+
+        const fs = require('fs');
+
+        let rawdata = fs.readFileSync('../review_course12/data/mockcomments.json');
+        let mockCommentS = JSON.parse(rawdata);
+        console.log(mockCommentS);
+
+        var filtered = _.where(mockCommentS, {id_article: 1});
+        console.log(filtered)
+
+        // const result = comment.findByArticletId(1);
+        
+    //     expect(result).toEqual([
+    //         {
+    //             "id_comment": 1,
+    //             "id_article": 1,
+    //             "content": "รีวิวดีมากเลยครับ",
+    //             "stars": "* * *",
+    //             "id_profile": 1,
+
+    //         },
+    //         {
+    //             "id_comment": 2,
+    //             "id_article": 1,
+    //             "content": "ขอบคุณมากๆ ค้าาา",
+    //             "stars": "* * *",
+    //             "id_profile": 1,
+
+    //         }
+    //     ]);
 
 
 //     });
