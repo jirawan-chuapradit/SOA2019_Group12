a76a63127a7d9ff36f225c7930343e736d8f2b3e
Adobe Beta <adobebeta1997@gmail.com>
2019-03-24 23:15:26 +0700

    Edit article test

--- review_course12/Test/articles.test.js
+++ review_course12/Test/articles.test.js
@@ -2,23 +2,48 @@
 
 const article = require("../articles");
 
-// describe("getAllArticles", () => {
-//     it("should return all article",  () => {
+describe("getAllArticles", () => {
+    it("should return all article",  () => {
 
-//         // const result = article.findAll();
+        // const result = article.findAll();
        
-//         'use strict';
+        'use strict';
 
-//         const fs = require('fs');
+        const fs = require('fs');
         
-//         let rawdata = fs.readFileSync('../review_course12/data/mockArticles.json');  
-//         let mockArticles = JSON.parse(rawdata);  
-//         console.log(mockArticles);
-//         expect(result).toEqual(mockArticles)
+        let rawdata = fs.readFileSync('../review_course12/data/mockarticles.json');  
+        let mockArticles = JSON.parse(rawdata);  
+        console.log(mockArticles);
+        expect(mockArticles).toEqual([
+            {
+                "id": 1,
+                "category": "สังคมศาสตร์",
+                "subject": "ECONOMICS FOR BUSINESS",
+                "stars": "* * * * *",
+                "description": "ดีมากๆ เเรียนเกี่ยวกับแนวคิดการทำงานของระบบเศษฐกิจอ่ะไม่มีเช็คชื่อ ส่งงานอย่างเดียว",
+                "grade": "A"
+            },
+            {
+                "id": 2,
+                "category": "สังคมศาสตร์",
+                "subject": "ENTREPRENEURSHIP",
+                "stars": "* * * *",
+                "description": "อาจารย์สอนดี แต่เนื้อหาเยอะ ไม่เหมาะกับคนที่ ไม่ชอบอ่านหนังสือ",
+                "grade": "A"
+            },
+            {
+                "id": 3,
+                "category": "ภาษา",
+                "subject": "ENGLISH FOR MARKETING",
+                "stars": "* *",
+                "description": "เน้นเวิร์คช้อป ไม่เข้าเรียนได้ ส่วนตัวคิดว่าดี แต่ได้ A ยาก",
+                "grade": "B"
+            }
+        ])
         
 
-//     });
-// });
+    });
+});
 
 
 describe("getArticlesByID", () => {
