3503527861f620208789a7babd1957cc1c3f7cc9
Nussaba Khienprasit <adobebeta1997@gmail.com>
2019-05-07 09:02:10 +0700

    refactor code Login

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -65,9 +65,7 @@ class Login extends React.Component {
       }
       
 
-      //   formIsValid = false;
-      //      errors["password"] = "*Please enter secure and strong password.";
-      // }
+      
 
       // if (typeof fields["password"] !== "undefined") {
       //   if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
